e5c4d8df4bb96556e19b0b317198dec329bb3267
trnqbao <tqbao2363@gmail.com>
2026-04-09 18:22:31 +0700

    chore: configure CORS middleware

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from utils import classify_doc
 from schemas import ClassifyRequest, ClassifyResponse
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST"],
+    allow_headers=["Content-Type"],
+)
 
 @app.post("/classify", response_model=ClassifyResponse)
 def classify(req: ClassifyRequest):
